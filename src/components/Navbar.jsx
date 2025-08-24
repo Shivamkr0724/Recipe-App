e99b8c4bfe13bef2982b4e6952a195f544395460
FinnBalorDJ <shivamkmr0724@gmail.com>
2025-08-24 16:08:42 +0530

    project completed

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+export default function Navbar(){
+    return(
+        <header>
+            <img src="./src/assets/chef-claude-icon.png" alt="" />
+            <h1>Chef Claude</h1>
+        </header>
+    )
+}
