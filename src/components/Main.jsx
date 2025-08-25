45a0a5a5e71288ec6da128ab7121358522531d00
FinnBalorDJ <shivamkmr0724@gmail.com>
2025-08-25 23:58:06 +0530

    fix scroll bug

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,7 @@ import ClaudeRecipe from "./ClaudeRecipe"
 import { getRecipeFromMistral } from "../ai"
 
 export default function Main() {
-    const [ingredients, setIngredients] = React.useState(["Saloni💗"])
+    const [ingredients, setIngredients] = React.useState([])
     
     const [recipe, setRecipe] = React.useState("")
 
